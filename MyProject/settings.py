3ebe95bbc165a329ba1f79d891c3e47a4366d016
codermahiuddin <codermahiuddin@gmail.com>
2021-06-09 12:22:02 +0600

    Change Settings

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -138,6 +138,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 #Media
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
